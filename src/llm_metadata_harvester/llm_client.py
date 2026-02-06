56b95d187b03efcb19027b07930603bf916fb83f
Zehao Lu <com3dian@outlook.com>
2026-02-06 10:22:02 +0100

    added surf llm support (#28)
    * added surf llm support
    
    * updated prefix-cleaning into more elegant way

--- src/llm_metadata_harvester/llm_client.py
+++ src/llm_metadata_harvester/llm_client.py
@@ -9,21 +9,32 @@ import os
 load_dotenv()
 
 class LLMClient:
-    def __init__(self, model_name: str, temperature: float = 0.0, api_key: str = None):
-        self.model = model_name
+    def __init__(self, model_name: str,
+                 temperature: float = 0.0,
+                 api_key: str = None, **kwargs):
+        self.temperature = temperature
         if model_name.startswith("gpt"):
-            self.temperature = temperature
+            self.model = model_name
             self.provider = "openai"
             key = api_key or os.getenv("OPENAI_API_KEY")
             self.client = OpenAI(api_key=key)
         elif model_name.startswith("gemini"):
+            self.model = model_name
             if genai is None:
                 raise ImportError("google package is required for Gemini models. Install it with: pip install google")
-            self.temperature = temperature
             self.provider = "gemini"
             key = api_key or os.getenv("GEMINI_API_KEY")
             self.client = genai.Client(api_key=key)
-
+        elif model_name.startswith("surf"):
+            # for surf models it should be named `surf-{actual_model_name}`
+            self.provider = "surf"
+            self.model = model_name.removeprefix("surf-")  # Remove 'surf-' prefix
+            key = api_key or os.getenv("SURF_API_KEY")
+            base_url = kwargs.get("base_url", "https://willma.surf.nl/api/v0")
+            self.client = OpenAI(
+                api_key=api_key,
+                base_url=base_url
+            )
         else:
             raise ValueError(f"Unsupported LLM: {model_name}")
 
@@ -41,5 +52,12 @@ class LLMClient:
                     model=self.model,
                     contents=messages[-1]["content"]
                 ).text
+            elif self.provider == "surf":
+                return self.client.chat.completions.create(
+                    model=self.model,
+                    messages=messages,
+                    temperature=self.temperature,
+                    max_tokens=max_tokens
+                ).choices[0].message.content
         except Exception as e:
             raise RuntimeError(f"LLM client response failed: Error from LLM provider:\n{e}") from e
